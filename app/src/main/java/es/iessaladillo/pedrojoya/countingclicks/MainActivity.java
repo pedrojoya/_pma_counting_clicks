68aff05e1b563f7c1123d01dd0cf1cb6163802ab
pedrojoya <pedrojoya@gmail.com>
2020-02-27 20:11:27 +0100

    v4_string_resource

--- app/src/main/java/es/iessaladillo/pedrojoya/countingclicks/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/countingclicks/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showTimes() {
-        b.lblTimes.setText(String.valueOf(times));
+        b.lblTimes.setText(times + " " + getString(R.string.main_times));
     }
 
 }
